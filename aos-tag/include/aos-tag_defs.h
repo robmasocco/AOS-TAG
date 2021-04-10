64c95bf1ebecd5136acd261815d9284d14526a7d
robmasocco <robmasocco@gmail.com>
2021-04-10 20:51:18 +0200

    Set up core module source files.

---
+++ aos-tag/include/aos-tag_defs.h
@@ -0,0 +1,22 @@
+/* 
+ * This is free software.
+ * You can redistribute it and/or modify this file under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this file; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+ */
+/*
+ * @brief Definition of common macros and data for this module.
+ *
+ * @author Roberto Masocco
+ *
+ * @date April 10, 2021
+ */
