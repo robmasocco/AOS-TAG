3775f72431640aa75dc66c383c7fa93fed3370aa
Roberto Masocco <robmasocco@gmail.com>
2021-04-18 14:35:12 +0200

    Added perror.

--- Tests/syscalls_test.c
+++ Tests/syscalls_test.c
@@ -29,13 +29,20 @@ int tag_ctl(int tag, int command) {
 
 int main(void) {
     int ret;
-    ret = tag_get(1024, 2, 1);
+    ret = tag_get(1024, 1, 1);
     printf("tag_get: %d.\n", ret);
+    perror("tag_get");
+    ret = tag_get(1024, 0, 1);
+    printf("tag_get: %d.\n", ret);
+    perror("tag_get");
     ret = tag_receive(200, 12, NULL, 4096);
     printf("tag_receive: %d.\n", ret);
+    perror("tag_receive");
     ret = tag_send(2345, 34, NULL, 5000);
     printf("tag_send: %d.\n", ret);
+    perror("tag_send");
     ret = tag_ctl(34566, 0);
     printf("tag_ctl: %d.\n", ret);
+    perror("tag_ctl");
     exit(EXIT_SUCCESS);
 }
