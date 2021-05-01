cae0a4e448c4074199da1c13f651920bd66a99a5
Roberto Masocco <robmasocco@gmail.com>
2021-05-01 14:13:04 +0200

    Talker/listener testers implemented.

---
+++ Tests/listener.c
@@ -0,0 +1,64 @@
+/**
+ * @brief TAG listener CLI application.
+ *
+ * @author Roberto Masocco <robmasocco@gmail.com>
+ *
+ * @date May 1, 2021
+ */
+
+#include "tests_vscode_settings.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/ipc.h>
+
+#include "../aos-tag/include/aos-tag.h"
+
+#define BUFLEN 80
+#define BUFSIZE (BUFLEN + 1)
+
+/* Signal handler: allows to gracefully terminate the process. */
+void term_handler(int sig) {
+    printf("\b\bGot signal: %d.\n", sig);
+}
+
+/* The works. */
+int main(int argc, char **argv) {
+    // Parse input arguments.
+    if (argc != 3) {
+        fprintf(stderr, "Usage: listener.out KEY LEVEL\n");
+        exit(EXIT_FAILURE);
+    }
+    int key = atoi(argv[1]);
+    int lvl = atoi(argv[2]);
+    if (key == IPC_PRIVATE)
+        fprintf(stderr, "ERROR: We're about to reopen a private instance...\n");
+    // Open the instance of the service.
+    int tag = tag_get(key, TAG_OPEN, TAG_USR);
+    if (tag == -1) {
+        fprintf(stderr, "ERROR: Failed to open tag service instance.\n");
+        perror("tag_get");
+        exit(EXIT_FAILURE);
+    }
+    printf("Opened instance with tag: %d.\n", tag);
+    // Start receiving messages.
+    char msg_buf[BUFSIZE];
+    for (;;) {
+        memset(msg_buf, 0, BUFSIZE);
+        if (tag_receive(tag, lvl, msg_buf, BUFSIZE)) {
+            if (errno != EINTR) {
+                fprintf(stderr, "ERROR: Failed to receive message.\n");
+                perror("tag_receive");
+                exit(EXIT_FAILURE);
+            } else break;
+        }
+        printf("%s\n", msg_buf);
+    }
+    // All done!
+    printf("Listener terminated!\n");
+    exit(EXIT_SUCCESS);
+}
---
+++ Tests/talker.c
@@ -0,0 +1,74 @@
+/**
+ * @brief TAG talker CLI application.
+ *
+ * @author Roberto Masocco <robmasocco@gmail.com>
+ *
+ * @date May 1, 2021
+ */
+
+#include "tests_vscode_settings.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+#include "../aos-tag/include/aos-tag.h"
+
+/* Signal handler: allows to gracefully terminate the process. */
+void term_handler(int sig) {
+    printf("\b\bGot signal: %d.\n", sig);
+}
+
+/* The works. */
+int main(int argc, char **argv) {
+    // Parse input arguments.
+    if (argc != 5) {
+        fprintf(stderr, "Usage: talker.out KEY LEVEL MESSAGE PERIOD\n");
+        fprintf(stderr, "\tPERIOD: Sleep time in milliseconds.\n");
+        exit(EXIT_FAILURE);
+    }
+    int key = atoi(argv[1]);
+    int lvl = atoi(argv[2]);
+    int period = atoi(argv[4]);
+    size_t bufsize = strlen(argv[3]) + 10 + 3 + 1;
+    char msg_buf[bufsize];
+    unsigned int msg_cnt = 0;
+    signal(SIGINT, term_handler);
+    // Open a new instance of the service.
+    int tag = tag_get(key, TAG_CREATE, TAG_USR);
+    if (tag == -1) {
+        fprintf(stderr, "ERROR: Failed to create new tag service instance.\n");
+        perror("tag_get");
+        exit(EXIT_FAILURE);
+    }
+    printf("Opened new instance with tag: %d.\n", tag);
+    // Start posting messages.
+    for (;;) {
+        memset(msg_buf, 0, bufsize);
+        sprintf(msg_buf, "[%u] %s", msg_cnt, argv[3]);
+        int send_res = tag_send(tag, lvl, msg_buf, strlen(msg_buf));
+        if (send_res != 0) {
+            if (errno != EINTR) {
+                fprintf(stderr, "ERROR: Failed to send message no. %u.\n",
+                        msg_cnt);
+                perror("tag_send");
+                exit(EXIT_FAILURE);
+            } else break;
+        }
+        printf("Sent message no. %u.\n", msg_cnt);
+        msg_cnt++;
+        if (usleep(period * 1000)) break;
+    }
+    // Try to remove the instance.
+    // If listeners are still active, this should fail.
+    if (tag_ctl(tag, REMOVE)) {
+        fprintf(stderr, "ERROR: Failed to remove tag service instance.\n");
+        perror("tag_ctl");
+        exit(EXIT_FAILURE);
+    }
+    printf("Talker done!\n");
+    exit(EXIT_SUCCESS);
+}
