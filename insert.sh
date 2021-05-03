45f3a76e2d851c0ad00312f7fb9a9994c4c17032
Roberto Masocco <robmasocco@gmail.com>
2021-05-03 17:17:36 +0200

    Default values are now available and a copy of the header is produced.

--- insert.sh
+++ insert.sh
@@ -9,7 +9,7 @@ usage() {
     echo "Usage: insert.sh MAX_TAGS MAX_MSG_SZ" 1>&2;
     echo "  MAX_TAGS: Max number of tags (will be passed to insmod)." 1>&2;
     echo "  MAX_MSG_SZ: Max allowed size of a message, in bytes (will be passed to insmod)." 1>&2;
-    exit 1;
+    echo "Values will now be set to defaults." 1>&2;
 }
 
 # Verify that the PWD is the project's root directory.
@@ -23,9 +23,14 @@ fi
 # Parse input arguments.
 if [[ $# -ne 2 ]]; then
     usage
+    MAX_TAGS=256
+    MAX_MSG_SZ=4096
+else
+    MAX_TAGS=$1
+    MAX_MSG_SZ=$2
 fi
-MAX_TAGS=$1
-MAX_MSG_SZ=$2
+echo "MAX_TAGS: $MAX_TAGS"
+echo "MAX_MSG_SZ: $MAX_MSG_SZ"
 
 # Compile modules.
 echo "Compiling modules..."
@@ -66,26 +71,28 @@ echo "tag_ctl system call installed at: $(cat /sys/module/aos_tag/parameters/tag
 echo "Device driver registered with major number: $(cat /sys/module/aos_tag/parameters/tag_drv_major)"
 
 # Generate userspace header.
+HEADER_NAME=aos-tag.h
+HEADER_PATH=aos-tag/include/$HEADER_NAME
 echo "Generating userspace header..."
-sed -i -e "s/#define __NR_tag_get 134/#define __NR_tag_get $(cat /sys/module/aos_tag/parameters/tag_get_nr)/" aos-tag/include/aos-tag.h
+sed "s/#define __NR_tag_get 134/#define __NR_tag_get $(cat /sys/module/aos_tag/parameters/tag_get_nr)/" $HEADER_PATH > $HEADER_NAME
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Failed to generate userspace header." 1>&2
     exit 1
 fi
-sed -i -e "s/#define __NR_tag_receive 174/#define __NR_tag_receive $(cat /sys/module/aos_tag/parameters/tag_receive_nr)/" aos-tag/include/aos-tag.h
+sed -i -e "s/#define __NR_tag_receive 174/#define __NR_tag_receive $(cat /sys/module/aos_tag/parameters/tag_receive_nr)/" $HEADER_NAME
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Failed to generate userspace header." 1>&2
     exit 1
 fi
-sed -i -e "s/#define __NR_tag_send 177/#define __NR_tag_send $(cat /sys/module/aos_tag/parameters/tag_send_nr)/" aos-tag/include/aos-tag.h
+sed -i -e "s/#define __NR_tag_send 177/#define __NR_tag_send $(cat /sys/module/aos_tag/parameters/tag_send_nr)/" $HEADER_NAME
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Failed to generate userspace header." 1>&2
     exit 1
 fi
-sed -i -e "s/#define __NR_tag_ctl 178/#define __NR_tag_ctl $(cat /sys/module/aos_tag/parameters/tag_ctl_nr)/" aos-tag/include/aos-tag.h
+sed -i -e "s/#define __NR_tag_ctl 178/#define __NR_tag_ctl $(cat /sys/module/aos_tag/parameters/tag_ctl_nr)/" $HEADER_NAME
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Failed to generate userspace header." 1>&2
     exit 1
 fi
-echo "Userspace header file location: aos-tag/include/aos-tag.h" aos-tag/include/aos-tag.h
+echo "Userspace header file name: $HEADER_NAME"
 echo "All done!"
